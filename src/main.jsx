81a9b5c0c2680d5a372c3e0b098a58c50a989420
SyedAlamin <syedalamin2580@gmail.com>
2023-04-04 23:00:27 +0600

    eract route

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,33 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Shop from './components/Shop/Shop';
+import Home from './components/Layout/Home';
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
+import Login from './components/Login/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Shop></Shop>
+    element: <Home></Home>,
+    children:[
+      {
+        path: '/',
+        element: <Shop></Shop>
+      },
+      {
+        path: 'orders',
+        element: <Orders></Orders>
+      },
+      {
+        path: 'inventory',
+        element: <Inventory></Inventory>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      }
+    ]
   }
 ])
 
