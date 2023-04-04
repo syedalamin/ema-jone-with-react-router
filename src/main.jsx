82f6ee4ce5d5d61f436e2795f58db401f0798af4
SyedAlamin <syedalamin2580@gmail.com>
2023-04-04 22:22:56 +0600

    ema-jone

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  Link,
+  RouterProvider,
+} from "react-router-dom";
+import Shop from './components/Shop/Shop';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Shop></Shop>
+  }
+])
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>,
 )
