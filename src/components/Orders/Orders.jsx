5d9e9a86a90d9390486c7ceab506c174c13f1196
SyedAlamin <syedalamin2580@gmail.com>
2023-04-05 16:14:52 +0600

    delete from cart :

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import RevewItem from '../RevewItem/RevewItem';
 import './Order.css'
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
-    const cart = useLoaderData();
+    const savedCart = useLoaderData();
+    const [cart , setCart] = useState(savedCart);
+
+    const handleRemoveFromCart = (id => {
+        const remaining = cart.filter(product => product.id !== id );
+        setCart(remaining);
+        removeFromDb(id);
+   
+    })
+
+
+
     return (
         <div className='shop-container'>
             <div className='review-container'>
@@ -13,6 +25,7 @@ const Orders = () => {
                 cart.map(product => <RevewItem
                     key={product.id}
                     product={product}
+                    handleRemoveFromCart={handleRemoveFromCart}
                 ></RevewItem>)
                }
             </div>
--- src/components/RevewItem/RevewItem.jsx
+++ src/components/RevewItem/RevewItem.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './RevewItem.css'
-const RevewItem = ({product}) => {
-    const {img, quantity, id, price, name} = product;
+const RevewItem = ({ product , handleRemoveFromCart}) => {
+    const { img, quantity, id, price, name } = product;
     return (
         <div className='review-item'>
-           <img src={img} alt="" />
+            <img src={img} alt="" />
+            <div className='review-details'>
+                <p className='product-title'>{name}</p>
+                <p>Price: <span className='oragne-text'>${price}</span></p>
+                <p>Quantity: <span className='oragne-text'>${quantity}</span></p>
+            </div>
+            <button onClick={() => handleRemoveFromCart(id)} className='btn-delete'>D</button>
         </div>
     );
 };
